d773d42a3191abbf7845b24b283c18d2fae7bf8a
Alex <85185873+lexmilanez@users.noreply.github.com>
2022-11-14 16:21:34 -0800

    Added Source Files

--- sort.cpp
+++ sort.cpp
@@ -1,3 +1,38 @@
 //
-// Created by Alexandro I. Milanez on 11/14/22.
 //
+//
+#include <fstream>
+#include <iostream>
+
+
+using namespace std::chrono;
+using namespace std;
+int main()
+{
+    ifstream inFile;
+    int rows;
+    inFile.open("input.txt");
+    inFile >> rows;
+    cout<<"rows is " <<rows << " ";
+    int arr[rows];
+    for (int a = 0; a < rows; a++)
+        inFile >> arr[a];
+
+    int n = sizeof(arr)/sizeof(arr[0]);
+
+    int insertionarr[rows];
+    int selectionarr[rows];
+    int merge[rows];
+    int bubble[rows];
+    int quick[rows];
+    for (int a = 0; a < rows; a++)
+    {
+        insertionarr[a] = arr[a];
+        selectionarr[a] = arr[a];
+        merge[a] = arr[a];
+        bubble[a] = arr[a];
+        quick[a] = arr[a];
+    }
+
+    return 0;
+}
